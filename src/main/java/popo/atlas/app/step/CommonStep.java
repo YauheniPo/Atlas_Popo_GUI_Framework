d7c71b97d40a1865f797b3f635923ee8354fc538
YauheniPo <ip.popovich@mail.ru>
2019-06-22 13:30:11 +0300

    Updated test and added new elements and test

--- src/main/java/popo/atlas/app/step/CommonStep.java
+++ src/main/java/popo/atlas/app/step/CommonStep.java
@@ -3,6 +3,7 @@ package popo.atlas.app.step;
 import popo.atlas.app.element.Header;
 import popo.atlas.app.page.MainPage;
 import popo.atlas.framework.base.BaseTest;
+import popo.atlas.framework.base.driver.Browser;
 import ru.yandex.qatools.allure.annotations.Step;
 
 import static ru.yandex.qatools.matchers.webdriver.DisplayedMatcher.displayed;
@@ -13,7 +14,7 @@ public class CommonStep extends BaseTest {
     protected void search(String repoTitleText) {
         MainPage mainPage = onPage(MainPage.class);
         Header header = mainPage.header();
-        header.searchInput().waitUntil(displayed()).sendKeys(repoTitleText);
+        header.searchInput().waitUntil(displayed(), Browser.TIMEOUT).sendKeys(repoTitleText);
         mainPage.header().searchInput().submit();
     }
 }
