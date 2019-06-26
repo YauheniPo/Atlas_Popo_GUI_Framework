a3f30d41d96152b43cb95b9184cb178f3b5faae7
YauheniPo <ip.popovich@mail.ru>
2019-06-26 11:26:26 +0300

    Upd common method 'search' to generic of pages

--- src/main/java/popo/atlas/app/element/Header.java
+++ src/main/java/popo/atlas/app/element/Header.java
@@ -1,20 +1,14 @@
 package popo.atlas.app.element;
 
+import io.qameta.atlas.core.api.Retry;
 import io.qameta.atlas.webdriver.AtlasWebElement;
 import io.qameta.atlas.webdriver.extension.FindBy;
-import popo.atlas.framework.base.driver.Browser;
 import ru.yandex.qatools.allure.annotations.Description;
-
-import static ru.yandex.qatools.matchers.webdriver.DisplayedMatcher.displayed;
 
 public interface Header extends AtlasWebElement<Header> {
 
     @Description("Search Input")
+    @Retry(timeout = 100000L)
     @FindBy(".//input[contains(@class, 'header-search-input')]")
     AtlasWebElement<Header> searchInput();
-
-    default AtlasWebElement searchInputWithWait() {
-        return searchInput().waitUntil(displayed(), Browser.TIMEOUT);
-    }
-
 }
