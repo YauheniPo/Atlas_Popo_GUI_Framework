089eb9c1306d7847883d93ab9e96faf897102ae1
YauheniPo <ip.popovich@mail.ru>
2019-07-06 14:16:46 +0300

    Optimized timeouts for elements

--- src/main/java/popo/atlas/app/element/Header.java
+++ src/main/java/popo/atlas/app/element/Header.java
@@ -4,9 +4,9 @@ import io.qameta.atlas.webdriver.AtlasWebElement;
 import popo.atlas.framework.atlas.extention.ContainsClass;
 import ru.yandex.qatools.allure.annotations.Description;
 
-public interface Header extends AtlasWebElement<Header> {
+public interface Header extends AtlasWebElement {
 
     @Description("Search Input")
     @ContainsClass(tag = "input", classValue = "header-search-input")
-    AtlasWebElement<Header> searchInput();
+    AtlasWebElement searchInput();
 }
