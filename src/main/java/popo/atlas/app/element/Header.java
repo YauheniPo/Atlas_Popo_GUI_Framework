23ca676a749a4193ee1359d65783401ab7fd45e3
YauheniPo <ip.popovich@mail.ru>
2019-07-06 13:34:12 +0300

    Optimized timeouts for elements

--- src/main/java/popo/atlas/app/element/Header.java
+++ src/main/java/popo/atlas/app/element/Header.java
@@ -1,6 +1,5 @@
 package popo.atlas.app.element;
 
-import io.qameta.atlas.core.api.Retry;
 import io.qameta.atlas.webdriver.AtlasWebElement;
 import popo.atlas.framework.atlas.extention.ContainsClass;
 import ru.yandex.qatools.allure.annotations.Description;
@@ -9,6 +8,5 @@ public interface Header extends AtlasWebElement<Header> {
 
     @Description("Search Input")
     @ContainsClass(tag = "input", classValue = "header-search-input")
-    @Retry(timeout = 60000L)
     AtlasWebElement<Header> searchInput();
 }
--- src/main/java/popo/atlas/app/step/CommonStep.java
+++ src/main/java/popo/atlas/app/step/CommonStep.java
@@ -1,21 +1,28 @@
 package popo.atlas.app.step;
 
 import io.qameta.atlas.webdriver.WebPage;
+import lombok.extern.log4j.Log4j2;
 import popo.atlas.app.element.Header;
 import popo.atlas.app.layout.WithHeader;
 import popo.atlas.app.page.SearchPage;
 import popo.atlas.framework.base.BaseTest;
 import ru.yandex.qatools.allure.annotations.Step;
 
+@Log4j2
 public class CommonStep extends BaseTest {
 
     @Step("Searching GitHub content: {1}")
-    protected <T extends WebPage & WithHeader> SearchPage search(Class<T> page, String repoTitleText) throws InterruptedException {
+    protected <T extends WebPage & WithHeader> SearchPage search(Class<T> page, String repoTitleText) {
         WebPage webPage = onPage(page);
         Header header = ((WithHeader)webPage).header();
-        header
-                .searchInput()
-                .wait(60000L);
+        try {
+            header
+                    .searchInput()
+                    .wait(60000L);
+        } catch (InterruptedException e) {
+            log.fatal(e);
+            e.printStackTrace();
+        }
         header
                 .searchInput()
                 .sendKeys(repoTitleText);
