ed072ff8990d99c2ec7f53aafbace9ecedfdfcb5
YauheniPo <ip.popovich@mail.ru>
2019-07-12 22:10:32 +0300

    Added Video recording and optimized matchers for waiting

--- src/main/java/popo/atlas/app/step/CommonStep.java
+++ src/main/java/popo/atlas/app/step/CommonStep.java
@@ -1,6 +1,8 @@
 package popo.atlas.app.step;
 
 import io.qameta.atlas.webdriver.WebPage;
+import lombok.extern.log4j.Log4j2;
+import org.openqa.selenium.WebElement;
 import popo.atlas.app.element.Header;
 import popo.atlas.app.layout.WithHeader;
 import popo.atlas.app.page.SearchPage;
@@ -10,18 +12,23 @@ import ru.yandex.qatools.allure.annotations.Step;
 import static ru.yandex.qatools.matchers.webdriver.EnabledMatcher.enabled;
 import static ru.yandex.qatools.matchers.webdriver.ExistsMatcher.exists;
 
+@Log4j2
 public class CommonStep extends BaseTest {
 
     @Step("Searching GitHub content: {1}")
     protected <T extends WebPage & WithHeader> SearchPage search(Class<T> page, String repoTitleText) {
         WebPage webPage = onPage(page);
         Header header = ((WithHeader)webPage).header();
-        header
+        String text = header
                 .searchInput()
-                .waitUntil(exists());
+                .waitUntil(exists()).getText();
+        log.info("text " + text);
         header
-                .searchInput().waitUntil(enabled())
-                .sendKeys(repoTitleText);
+                .searchInput().getWrappedElement().sendKeys(repoTitleText);
+        log.info("page " + getWebDriver().getPageSource());
+        header.searchInput().sendKeys(repoTitleText);
+        log.info("page " + getWebDriver().getPageSource());
+
         header
                 .searchInput()
                 .submit();
