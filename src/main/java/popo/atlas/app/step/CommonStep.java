e0f521bddd7509ac80351940cf84578235fcf62a
YauheniPo <ip.popovich@mail.ru>
2019-07-13 18:22:21 +0300

    Added Video recording and optimized matchers for waiting

--- src/main/java/popo/atlas/app/step/CommonStep.java
+++ src/main/java/popo/atlas/app/step/CommonStep.java
@@ -1,7 +1,6 @@
 package popo.atlas.app.step;
 
 import io.qameta.atlas.webdriver.WebPage;
-import lombok.extern.log4j.Log4j2;
 import popo.atlas.app.element.Header;
 import popo.atlas.app.layout.WithHeader;
 import popo.atlas.app.page.SearchPage;
@@ -11,14 +10,12 @@ import ru.yandex.qatools.allure.annotations.Step;
 
 import static ru.yandex.qatools.matchers.webdriver.DisplayedMatcher.displayed;
 
-@Log4j2
 public class CommonStep extends BaseTest {
 
     @Step("Searching GitHub content: {1}")
     protected <T extends WebPage & WithHeader> SearchPage search(Class<T> page, String searchText) {
         WebPage webPage = onPage(page);
         Header header = ((WithHeader)webPage).header();
-        log.info(header.getWrappedElement());
         header
                 .searchInput().waitUntil(displayed(), Browser.TIMEOUT)
                 .sendKeys(searchText);
