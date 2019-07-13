2d81737a1b9ab558b2e807abec1009837e8445da
YauheniPo <ip.popovich@mail.ru>
2019-07-14 00:25:41 +0300

    Added Video recording and optimized matchers for waiting

--- src/main/java/popo/atlas/app/element/Header.java
+++ src/main/java/popo/atlas/app/element/Header.java
@@ -1,8 +1,6 @@
 package popo.atlas.app.element;
 
-import io.qameta.atlas.core.api.Retry;
 import io.qameta.atlas.webdriver.AtlasWebElement;
-import org.openqa.selenium.ElementNotInteractableException;
 import popo.atlas.framework.atlas.extention.ContainsClass;
 import ru.yandex.qatools.allure.annotations.Description;
 
@@ -10,6 +8,5 @@ public interface Header extends AtlasWebElement {
 
     @Description("Search Input")
     @ContainsClass(tag = "input", classValue = "header-search-input")
-    @Retry(ignoring = ElementNotInteractableException.class, timeout = 200000L)
     AtlasWebElement searchInput();
 }
