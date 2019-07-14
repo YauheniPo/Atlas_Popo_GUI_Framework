eabf11de3eed8cdba9bcaa4c0a05868e5697f6c4
YauheniPo <ip.popovich@mail.ru>
2019-07-15 01:26:53 +0300

    Added Video recording and optimized matchers for waiting

--- src/main/java/popo/atlas/app/layout/WithHeader.java
+++ src/main/java/popo/atlas/app/layout/WithHeader.java
@@ -1,6 +1,5 @@
 package popo.atlas.app.layout;
 
-import io.qameta.atlas.core.api.Retry;
 import popo.atlas.app.element.Header;
 import popo.atlas.framework.atlas.extention.ContainsClass;
 import ru.yandex.qatools.allure.annotations.Description;
@@ -9,7 +8,6 @@ public interface WithHeader {
 
     @Description("Header")
     @ContainsClass(tag = "header", classValue = "Header")
-    @Retry(timeout = 100000L)
     Header header();
 
 }
